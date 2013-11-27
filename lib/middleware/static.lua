1f6d6f577db52fcb0cd9487e86065ffd0e84c43c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-27 13:04:27 +0200

    simplify and finish static middleware

--- lib/middleware/static.lua
+++ lib/middleware/static.lua
@@ -5,15 +5,14 @@ local url = require('url')
 local mime = require('mime')
 local os = require('os')
 local string = require('string')
+local debug = require('debug')
 
 -- static files middleware
--- root - directory path
+-- root - directory path required
 -- options:
 --  `maxAge` - browser cache maxAge in milliseconds, defaults to 0
 --  `index` - default file name, defaults to 'index.html'
---  `autoIndex` - create autoindex page if no index, defaults to true
---  `redirectFolders` - redirect on folders, defaults to true
---  `dot` - allow dot-files to be fetched normally, defaults to false
+--  `hidden` - allow transfer of hidden files, defaults to false
 
 function static (root, options)
 	if not root then error('root is required') end
@@ -27,41 +26,16 @@ function static (root, options)
 	return function (req, res, follow)
 		if req.method ~= 'GET' and req.method ~= 'HEAD' then return follow() end
 
-		-- check freshness of `req` and `res` headers
-		-- returns boolean, false indicate that the cache is now stale
-		local function isFresh ()
-			-- defaults
-			local etagMatches = true
-			local notModified = true
-
-			local modifiedSince = req['if-modified-since']
-			local noneMatch = req['if-none-match']
-			local lastModified = res['last-modified']
-			local etag = res['etag']
-			local cc = req['cache-control']
-
-			-- unconditional request
-			if not modifiedSince and not noneMatch then return false end
-
-			-- check for no-cache
-			if cc and string.find(cc, 'no%-cache') then return false end
-
-			if modifiedSince then
-				modifiedSince = os.date(modifiedSince)
-				lastModified = os.date(lastModified)
-				notModified = lastModified <= modifiedSince
-			end
-
-			return etagMatches and notModified
-		end
-
-
-		local function serveFiles (route, fallback)
+		local function serveFiles (route)
 			fs.open(route, 'r', function (err, fd)
-				if err then end
+				if err then return follow() end
 
 				fs.fstat(fd, function (err, stat)
-					if err then end
+					if err then
+						fs.close(fd)
+						res:writeHead(500, {})
+						return res:finish(tostring(err) .. '\n' .. debug.traceback() .. '\n')
+					end
 
 					local headers
 					local code = 200
@@ -77,26 +51,21 @@ function static (root, options)
 						['Cache-Control'] = 'public, max-age=' .. (options.maxAge / 1000)
 					}
 
-					-- TO DO: support caching
-
-					if route:sub(#path) == '/' then
-					else
-						if stat.is_directory then
-							-- redirection is turned off
-							if not options.redirectFolders then return follow() end
-
-							fs.close(fd)
-							res:writeHead(302, { ['Location'] = req.url .. '/' })
-							return res:finish()
-						end
-
-
-						res:writeHead(code, headers)
-
-						if req.method == "HEAD" then return follow() end
-
-						fs.createReadStream(route, { fd = fd }):pipe(res)
+					-- skip directories and hidden files if no option specified
+					if stat.is_directory or not options.hidden and '.' == path.basename(route):sub(1, 1) then
+						fs.close(fd)
+						return follow()
 					end
+
+					res:writeHead(code, headers)
+
+					if req.method == "HEAD" or code == 304 then
+						fs.close(fd)
+						req.setCode = code
+						return res:finish()
+					end
+
+					fs.createReadStream(route, { fd = fd }):pipe(res)
 				end)
 			end)
 		end
@@ -104,7 +73,7 @@ function static (root, options)
 		local urlPath = path.join(root, req.url)
 
 		if options.index and urlPath:sub(#urlPath) == '/' then
-			serveFiles(path.join(urlPath, options.index), urlPath)
+			serveFiles(path.join(urlPath, options.index))
 		else
 			serveFiles(urlPath)
 		end
