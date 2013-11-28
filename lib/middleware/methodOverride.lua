f33f8e3ea77b0b6ea503c941a2cc5a3dc73be467
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-28 22:21:31 +0200

    more on methodoverride

--- lib/middleware/methodOverride.lua
+++ lib/middleware/methodOverride.lua
@@ -1,5 +1,7 @@
+local helpers = require('../helpers')
 
 -- provides faux http method support
+-- pass optional key param to use when checking for a method override, defaults to '_method'
 
 function methodOverride (key)
 	key = key or '_method'
@@ -7,7 +9,20 @@ function methodOverride (key)
 	req.originalMethod = req.originalMethod or req.method
 
 	return function (req, res, next)
-		-- body
+		local method
+
+		if req.body and type(req.body) == 'table' and req.body[key] ~= nil then
+		end
+
+		if req.headers['x-http-method-override'] then
+			method = req.headers['x-http-method-override']:lower()
+		end
+
+		if helpers.supports(method) then
+			req.method = method:upper()
+		end
+
+		follow()
 	end
 end
 
