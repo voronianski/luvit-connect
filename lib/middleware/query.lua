013ed2503ced3b3c2282b2734a29d1ecd6a19be4
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-29 14:09:05 +0200

    remove unnecessary options

--- lib/middleware/query.lua
+++ lib/middleware/query.lua
@@ -3,9 +3,7 @@ local qs = require('querystring')
 
 -- automatically parse the query-string when available to `req.query` table
 
-function query (options)
-	options = options or {}
-
+function query ()
 	return function (req, res, follow)
 		if not req.query then
 			if req.url:find('%?') then
