2f1b3ce7d34a9f38f5d267ffd36487848050e912
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-29 13:46:19 +0200

    add query middleware

---
+++ lib/middleware/query.lua
@@ -0,0 +1,26 @@
+local url = require('url')
+local qs = require('querystring')
+
+-- automatically parse the query-string when available to `req.query` table
+
+function query (options)
+	options = options or {}
+
+	return function (req, res, follow)
+		if not req.query then
+			if req.url:find('%?') then
+				local urlParsed = req._parsedUrl
+
+				if not urlParsed or urlParsed.href ~= req.url then
+					urlParsed = url.parse(req.url)
+				end
+
+				req.query = qs.parse(urlParsed.query)
+			end
+		end
+
+		follow()
+	end
+end
+
+return query
