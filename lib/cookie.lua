a2fe8002551b1d1fe8bc0cbee6d74b0a3051a483
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-12-06 18:24:30 +0200

    finish cookie module

--- lib/cookie.lua
+++ lib/cookie.lua
@@ -1,16 +1,13 @@
 -- Cookie module
--- TO DO: extract to separate module luvit module
+-- TO DO: extract to separate luvit module
 
+local os = require('os')
 local qs = require('querystring')
+local table = require('table')
 local Object = require('core').Object
 local helpers = require('./helpers')
 
 local Cookie = Object:extend()
-
-function Cookie:initialize ( ... )
-	self.w = 12
-	self.h = 10
-end
 
 function Cookie.meta.__tostring ()
 	return '<Cookie>'
@@ -18,33 +15,44 @@ end
 
 function Cookie:serialize (name, value, options)
 	options = options or {}
+	options.encode = options.encode or qs.urlencode
 
+	local cookiePairs = { name .. '=' .. options.encode(value) }
+
+	if options.maxAge then table.insert(cookiePairs, 'Max-Age=' .. options.maxAge) end
+	if options.domain then table.insert(cookiePairs, 'Domain=' .. options.domain) end
+	if options.path then table.insert(cookiePairs, 'Path=' .. options.path) end
+	if options.expires then table.insert(cookiePairs, 'Expires=' .. os.date('!%a, %d %b %Y %H:%M:%S GMT', options.expires)) end
+	if options.httpOnly then table.insert(cookiePairs, 'HttpOnly') end
+	if options.secure then table.insert(cookiePairs, 'Secure') end
+
+	return table.concat(cookiePairs, '; ')
 end
 
 function Cookie:parse (str, options)
 	options = options or {}
-	options.decode = options.decode or helpers.decodeURI
+	options.decode = options.decode or qs.urldecode
 
 	local tbl = {}
 	local cookiePairs = helpers.split(str, '%;%,')
 
-	for index, pair in pairs(cookiePairs) do
+	table.foreach(cookiePairs, function (index, pair)
 		local eqIndex = helpers.indexOf(pair, '=')
 
-		if eqIndex then
-			local key = pair:sub(1, eqIndex-1)
-			local val = pair:sub(eqIndex+1, #pair)
+		if not eqIndex then return end
 
-			-- quoted values
-			if val:find('%"', 1) == 1 then
-				val = val:sub(2, #val-1)
-			end
+		local key = pair:sub(1, eqIndex-1)
+		local val = pair:sub(eqIndex+1, #pair)
 
-			if not tbl[key] then
-				tbl[key] = options.decode(val)
-			end
+		-- quoted values
+		if val:find('%"', 1) == 1 then
+			val = val:sub(2, #val-1)
 		end
-	end
+
+		if not tbl[key] then
+			tbl[key] = options.decode(val)
+		end
+	end)
 
 	return tbl
 end
--- lib/middleware/cookieParser.lua
+++ lib/middleware/cookieParser.lua
@@ -3,8 +3,6 @@ local Cookie = require('../cookie')
 
 local cookie = Cookie:new()
 
-p(cookie:parse('foo="bar";ddd;age=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82'))
-
 -- parse cookie header and populate 'req.cookies'
 
 function cookieParser (secret, options)
